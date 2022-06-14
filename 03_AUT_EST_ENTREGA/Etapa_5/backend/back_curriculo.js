621d14e53871bb2d2ab75c074de0571a29e70f22
Mateus Rafael <mateus.silva@sou.inteli.edu.br>
2022-06-14 09:54:20 -0300

    Atividade de Personalização

--- 03_AUT_EST_ENTREGA/Etapa_5/backend/back_curriculo.js
+++ 03_AUT_EST_ENTREGA/Etapa_5/backend/back_curriculo.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const hostname = '127.0.0.3';
-const port = 3052;
+const port = (3052, 3062, 3072);
 const sqlite3 = require('sqlite3').verbose();
 const DBPATH = 'dbCurriculo.db';
 
